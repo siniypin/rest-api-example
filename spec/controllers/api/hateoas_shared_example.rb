c39a22d4d32c2b0fc0c93a9409f486ae25cd2b1e
Robert <siniypin@gmail.com>
2015-09-30 11:04:46 +0200

    Update hateoas_shared_example.rb

--- spec/controllers/api/hateoas_shared_example.rb
+++ spec/controllers/api/hateoas_shared_example.rb
@@ -1,17 +1,16 @@
 shared_context :comply_with_hateoas do |params = {id: 1, format: :json}|
-  it 'should reply with HAL-like structure on index' do
+  before do
     request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials('name', 'password')
     allow(User).to receive(:find_by_name).and_return(double(authenticate: true))
-
+  end
+  
+  it 'should reply with HAL-like structure on index' do
     get :index, params.reject { |k, v| k == :id }
 
     expect(response.body).to include '"links":'
   end
 
   it 'should reply with HAL-like structure on show' do
-    request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials('name', 'password')
-    allow(User).to receive(:find_by_name).and_return(double(authenticate: true))
-
     get :index, params.reject { |k, v| k == :id }
 
     expect(response.body).to include '"links":'
